93e753b55b42ed14d732459c546585325905e341
Rémi Cailletaud <remi.cailletaud@univ-grenoble-alpes.fr>
2024-09-27 11:25:44 +0200

    fix healthz

--- docker/nginx-unit/healthz.php
+++ docker/nginx-unit/healthz.php
@@ -7,10 +7,12 @@ $mysqli = new mysqli($dbHost, $dbUser, $dbPass, $dbDb);
 catch (Exception $e) {
     http_response_code(503);
     printf("Cant connect to GRR database", $e);
+    exit;
 }
 if (mysqli_connect_errno()) {
   http_response_code(503);
   printf("Cant connect to GRR database", mysqli_connect_error());
+  exit;
 }
 else {
 header("Content-Type: text/plain");
