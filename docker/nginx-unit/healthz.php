0aaf43bb4308bebb2e668c7f5722d0f0a6c015df
Rémi Cailletaud <remi.cailletaud@univ-grenoble-alpes.fr>
2024-09-27 10:57:34 +0200

    fix healthz

--- docker/nginx-unit/healthz.php
+++ docker/nginx-unit/healthz.php
@@ -1,17 +1,18 @@
 <?php
 include "../personnalisation/connect.inc.php";
-
+error_reporting(0);
 try {
 $mysqli = new mysqli($dbHost, $dbUser, $dbPass, $dbDb);
 }
 catch (Exception $e) {
     http_response_code(503);
-    echo 'Caught exception: ',  $e->getMessage(), "\n";
+    echo "Fail";
     exit;
 }
 if (mysqli_connect_errno()) {
   http_response_code(503);
-  printf("Cant connect to GRR database", mysqli_connect_error());
+  echo "Fail";
+  exit;
 }
 else {
 header("Content-Type: text/plain");
